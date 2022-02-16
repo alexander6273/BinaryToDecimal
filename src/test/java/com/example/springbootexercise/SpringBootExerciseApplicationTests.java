d50730f90e23d83a73f48baaa782799a9fb3246f
alexander6273 <alex6273@stud.kea.dk>
2022-02-16 21:15:45 +0100

    Initial commit

---
+++ src/test/java/com/example/springbootexercise/SpringBootExerciseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.springbootexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExerciseApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
